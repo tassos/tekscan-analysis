dd9dde69bde8837dac3cce1af95c9b54ec5b77d9
Tassos 'nakano' Natsakis <tassos.natsakis@mech.kuleuven.be>
2013-04-09 14:54:56 +0200

    New feature added: TekScan calibration code
    A new data aquisition device is added in the analysis code: the TekScan
    handle. The sensors of TekScan come uncalibrated, that's why I first need
    to aquire some measurements with a known load applied on the sensor and do
    a curve fitting on the measurements. This is what this code is doing.
    
    I should also code something that converts the TekScan raw measurements to
    measurements with Units, once you apply the calibration matrix of each
    sensor.

---
+++ Calibration.m
@@ -0,0 +1,79 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%
+%                              TekScan sensor calibration
+%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%
+% Function: Calibrating TekScan measurement files based on calibration
+% measurements.
+%
+% Input: none, the software opens a file dialog for loading the calibration
+% measurements and the actual measurements
+%
+% Output: Gives the actual measurement files calibrated.
+%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+function Calibration
+
+clear all
+
+%Read file
+[fileName,pathName,filterIndex] = uigetfile('.asf','Select calibration measurement files','MultiSelect','on');
+fileName=char(fileName);
+
+%Defining number of rows and columns of the sensor
+nrows=46;
+ncols=32;
+
+%Initialising arrays to gain speed
+calibration=zeros(size(fileName,1),nrows,ncols);
+loads=zeros(size(fileName,1),1);
+
+%Importing calibration data and inserting in a 3dimensional array. The
+%first dimension are the rows of the sensor, the second are the columns and
+%the third are for the different loading levels
+for i=1:size(fileName,1)
+    text = fileread([pathName fileName(i,:)]);
+    colsindex=regexp(text,'COLS');
+    rowsindex=regexp(text,'ROWS');
+    endframe=regexp(text,'END_FRAME');
+    senselarea=regexp(text,'SENSEL_AREA');
+%     fid = fopen([pathName fileName(i,:)]);
+%     inputArray=textscan(fid,'%d','Delimiter',',','HeaderLines',30);
+%     calibration(i,:,:)=reshape(inputArray{1},ncols,nrows)';
+%     fclose(fid);
+%     loads(i,1)=str2double(fileName(i,1:length(regexp(fileName(i,:),'\d'))));
+end
+
+%Define the quadratic equation that we'll use for fitting our data
+function F = myfun(x,xdata)
+    F = x(1)*xdata.^3+x(2)*xdata.^2+x(3)*xdata+x(4);
+end
+
+%Check to see if calibration with this sensor is already made. If
+%calibration file doesn't exists, go on with calculating the fitting
+%coefficients.
+if (exist([pathName 'calibration.mat'],'file')==2);
+    load([pathName 'calibration.mat'],'x');
+else
+    %Initialising arrays to gain speed
+    x.a=zeros(nrows,ncols);
+    x.b=zeros(nrows,ncols);
+    x.c=zeros(nrows,ncols);
+    x.d=zeros(nrows,ncols);
+    
+    %Fitting our data and storing them in the X array.
+    for i=1:nrows
+        for j=1:ncols
+            temp = lsqcurvefit(@myfun,[0;0.00015;0.64;-3.3e2],loads,calibration(:,i,j))';
+            x.a(i,j)=temp(1);
+            x.b(i,j)=temp(2);
+            x.c(i,j)=temp(3);
+            x.d(i,j)=temp(4);
+        end
+    end
+    save([pathName 'calibration.mat'], 'x');
+end
+
+end
