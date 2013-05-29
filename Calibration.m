ad55f9e65287745d383d70d81033a65e22de4b55
Tassos 'nakano' Natsakis <tassos.natsakis@mech.kuleuven.be>
2013-05-29 10:58:01 +0200

    Faster evaluation of user defined polynomial
    The 'polyval' function is used to evaluate the polynomial, so that it can
    be defined by the user using the 'order' prompt

--- Calibration.m
+++ Calibration.m
@@ -19,7 +19,6 @@ function Calibration
 clear all
 close all
 
-
 global xdata ydata
 
 %Getting screen size for calculating the proper position of the figures
@@ -30,17 +29,13 @@ scnsize = get(0,'ScreenSize');
 [measFileName,measPathName] = uigetfile('.asf','Select measurement files','MultiSelect','on');
 measFileName=char(measFileName);
 
-%Define the cubic equation that we'll use for fitting our data
+%Define the polynomial that we'll use for fitting our data. The order
+%of the polynomial is desided by the size of our x, which is decided by
+%the input 'order' that is user defined
 function F = FitFun(x)
-    resid = x(1)*(xdata).^3+x(2)*(xdata).^2+x(3)*(xdata)+x(4)-ydata;
-    F = resid.*[0.5;2;ones(size(resid,1)-2,1)];
+    resid = polyval(x,xdata) - ydata;
+    F = resid.*[2;ones(size(resid,1)-1,1)];
 end
-
-
-%In Auto-mode, the polynomial for the curve fitting, can be reconstructed
-%by itself from the following function. However, it is horribly much slower
-%to evaluate itself
-%FitFun= @(x,xdata) (subs(poly2sym(x,'y'),xdata));
 
 %Opening file
 text = fileread(strtrim([measPathName measFileName(1,:)]));
@@ -62,16 +57,16 @@ else
     %Read calibration files
     [calibFileName,calibPathName] = uigetfile('.asf','Select folder containing calibration measurement files','MultiSelect','on');
     calibFileName=char(calibFileName);
-    
+
     %Defining loading area
     prompt = {'Enter loading area width (mm):','Enter loading area length (mm):'};
     dimensions = str2double(inputdlg(prompt,'Input',1,{'40','40'}));
     loadArea = dimensions(1)*dimensions(2)*1e-6;
-    
+
     %Choose order of polynomial to be used for the fitting curve
     prompt = {'Choose order for the fitted polynomial'};
     order = str2double(inputdlg(prompt,'Input',1,{'3'}));
-    
+
     %Importing calibration data and inserting in a 3dimensional array. The
     %first dimension are the rows of the sensor, the second are the columns and
     %the third are for the different loading levels
@@ -107,47 +102,47 @@ else
 
     %Defining a counter for the progress bar
     prog=0;
-    
+
     %Positioning the figures in a nice array
     pos1 = [0, scnsize(4) * (1/3), scnsize(3)/2, 2*scnsize(4)/3];
     pos2 = [scnsize(3)/2, pos1(2), pos1(3), pos1(4)];
     fig1 =figure(1);
     set(fig1,'OuterPosition',pos1);
     waitBarPos=get(h,'OuterPosition');
-    set(h,'OuterPosition',[(scnsize(3)+waitBarPos(3))/2,(scnsize(4)/3+waitBarPos(4)/2) ,waitBarPos(3), waitBarPos(4)]);
-    
+    set(h,'OuterPosition',[pos2(1)/2, pos1(2)/3, waitBarPos(3), waitBarPos(4)]);
+
     for sens = fieldnames(index)'
         sensit=sens{1};
-        
+
         % Sorting the two arrays for Linux mode compatability.
         loads.(sensit)=sort(loads.(sensit));
         meanData.(sensit)=sort(meanData.(sensit));
-        
+
         %Defining the range of the the fiting curve
         t0=min(meanData.(sensit)(:)):1:max(meanData.(sensit)(:));
-        
+
         %Defining upper and lower boundary limits, and also the initial
         %values for the Least-Square fitting
         lb=-Inf(1,order+1);
         ub=Inf(1,order+1);
-        xo=zeros(1,order+1);
-        
-        xdata=[0;meanData.(sensit)];
-        ydata=[0;loads.(sensit)];
-        
+        xo=ones(1,order+1);
+
+        xdata=[meanData.(sensit)];
+        ydata=[loads.(sensit)];
+
         problem = createOptimProblem('lsqnonlin','x0',xo,'objective',@FitFun,'lb',lb,'ub',ub);%,'xdata',[meanData.(sensit)],'ydata',[loads.(sensit)]);
         ms = MultiStart('PlotFcns',{@gsplotfunccount,@gsplotbestf},'UseParallel','always');
         [x.(sensit),error.(sensit)]=run(ms,problem,50);
         gEval = gcf;
         set(gEval,'OuterPosition',pos2);
-        
+
         figure(1)
         hold on
         % Plotting for confirming least squares convergence
         scatter([meanData.(sensit)],[loads.(sensit)],'b');
         ycub=double(subs(poly2sym(x.(sensit),'t'),t0));
         plot(t0,ycub,'r','LineWidth',2);
-        
+
         %Updating progress bar
         prog=prog+1;
         waitbar(prog/6,h,'Calculating calibration coefficients');
@@ -165,10 +160,10 @@ for i=1:size(measFileName,1)
     sensit = regexp(text,'(?<=SENSITIVITY )\S*','match');
     sensit = strrep(sensit{1},'-','');
     calibratedData=zeros(endFrame,nrows,ncols);
-       
+
     for j=startFrame:endFrame
         waitbar(((i-1)*endFrame+j)/(endFrame*size(measFileName,1)),h,'Generating calibrated measurements');
-        
+
         %Reading the data from the correct frame
         rawData=regexp(text,['(?<=Frame ' num2str(j) '\r\n)((\d*,\d*)*\r\n)*'],'match');
         cellData=textscan(rawData{1},'%f','Delimiter',',');
