3214c286b3799a6a19974311d450f6a88636b3f8
Tassos (nakano) Natsakis <tassos.natsakis@mech.kuleuven.be>
2013-09-13 11:56:04 +0200

    New method for analysis multiple calibrations of sensors

---
+++ calibrationsComparison.m
@@ -0,0 +1,16 @@
+function calibrationsComparison
+    initialFolder = 'C:/users/u0074517/Documents/PhD/Foot-ankle project/Measurements';
+    directories = uipickfiles('FilterSpec',initialFolder);
+    
+    h=waitbar(0,'Initialising waitbar...');
+    
+    for z=1:size(directories,2)
+        coleur = hsv(size(directories,2));
+        [meanData,loads]=readCalibrationFiles(h,directories{z},1);
+        for sens=fieldnames(meanData)'
+            sensit = sens{1};
+            scatter([meanData.(sensit)],[loads.(sensit)],10,coleur(z,:));
+            hold on
+        end
+    end
+end
