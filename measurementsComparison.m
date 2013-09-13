fda1a9f19e04d80592bfdbfdd30fd4049325b131
Tassos (nakano) Natsakis <tassos.natsakis@mech.kuleuven.be>
2013-09-13 15:59:27 +0200

    Plotting and comparing different measurements

--- measurementsComparison.m
+++ measurementsComparison.m
@@ -19,17 +19,37 @@ function measurementsComparison
     [measFileName,measPathName] = uigetfile('.mat','Select measurement files',...
         'MultiSelect','on','C:\users\u0074517\Documents\PhD\Foot-Ankle Project\Measurements');
     
+    if ~iscell(measFileName)
+        measFileName={measFileName};
+    end
+    
     %Calculate the size of the array data. First dimension is for different
     %measurements, the rest are following the same convention as all the
     %Tekscan related files.
     load([measPathName measFileName{1}],'calibratedData');
-    data=squeeze(zeros([size(measFileName),size(calibratedData)]));
+    data=zeros([size(measFileName),size(calibratedData)]);
     
     for i=1:size(measFileName,2)
         load([measPathName measFileName{i}],'calibratedData');
-        data(i,:,:,:) = calibratedData;
+        data(1,i,:,:,:) = calibratedData;
     end
     
     %Now we can do statistics
+    meanMeas=squeeze(mean(data,2));
+    sdMeas=squeeze(std(data,0,2));
+    
+    %Define a grid to plot the results
+    x=repmat(1:size(data,4),size(data,5),1)';
+    y=repmat(1:size(data,5),size(data,4),1);
+    
+    plot3dErrorbars(x(:),y(:),meanMeas(1,:),sdMeas(1,:));
+    h = uicontrol('style','slider','units','pixel','position',[20 20 300 20]);
+    addlistener(h,'ActionEvent',@(hObject, event) makeplot(hObject,x,y,meanMeas,sdMeas));
+
+    function makeplot(hObject,x,y,meanMeas,sdMeas)
+        n = ceil(get(hObject,'Value')*100+1);
+        plot3dErrorbars(x(:),y(:),meanMeas(n,:),sdMeas(n,:));
+        refreshdata;
+    end
 
 end
---
+++ plot3dErrorbars.m
@@ -0,0 +1,48 @@
+function [h]=plot3dErrorbars(x, y, z, e)
+    % Source: http://code.izzid.com/2007/08/19/How-to-make-a-3D-plot-with-errorbars-in-matlab.html
+
+    % This matlab function plots 3d data using the plot3 function
+    % it adds vertical errorbars to each point symmetric around z
+    % I experimented a little with creating the standard horizontal hash
+    % tops the error bars in a 2d plot, but it creates a mess when you 
+    % rotate the plot
+    %
+    % x = xaxis, y = yaxis, z = zaxis, e = error value
+
+    % create the standard 3d scatterplot
+    hold off;
+    h=plot3(x, y, z, '.k');
+
+    % looks better with large points
+    set(h, 'MarkerSize', 25);
+    hold on
+
+    % now draw the vertical errorbar for each point
+    for i=1:length(x)
+            xV = [x(i); x(i)];
+            yV = [y(i); y(i)];
+            zMin = z(i) + e(i);
+            zMax = z(i) - e(i);
+
+            zV = [zMin, zMax];
+            % draw vertical error bar
+            h=plot3(xV, yV, zV, '-k');
+            set(h, 'LineWidth', 2);
+    end
+    
+    % now we want to fit a surface to our data
+    % the  0.25 and 0.1 define the density of the fit surface
+    % adjust them to your liking
+    tt1=floor(min(min(x))):0.25:max(max(x));
+    tt2=floor(min(min(y))):0.1:max(max(y));
+
+    % prepare for fitting the surface
+    [xg,yg]=meshgrid(tt1,tt2);
+
+    % fit the surface to the data; 
+    % matlab has several choices for the fit;  below is "linear"
+    zg=griddata(x, y, z, xg,yg,'linear');
+    % draw the mesh on our plot
+    mesh(xg,yg,zg), xlabel('x axis'), ylabel('y axis'), zlabel('z axis')
+end
+
