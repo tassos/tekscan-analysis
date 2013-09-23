e754c62eafa1515ad03514b2cccd13437b8dbc73
Tassos (nakano) Natsakis <tassos.natsakis@mech.kuleuven.be>
2013-09-23 15:12:50 +0200

    Changes in plotting of 3d pressure measurements
    The plotting of the error bars in each point is disabled, cause it's very
    difficult to intrepret. Furthermore, a new feature is developed so that
    the rotation of the graph remains when the user is browsing through the
    time steps.

--- plot3dErrorbars.m
+++ plot3dErrorbars.m
@@ -8,37 +8,37 @@ function [h]=plot3dErrorbars(x, y, z, e)
     % rotate the plot
     %
     % x = xaxis, y = yaxis, z = zaxis, e = error value
-
-    % create the standard 3d scatterplot
-    hold off;
-    h=plot3(x(:), y(:), z(:), '.k');
-
-    % looks better with large points
-    set(h, 'MarkerSize', 25);
-    hold on
-
-    % now draw the vertical errorbar for each point
-    zMin = z + e;
-    zMax = z - e;
-
-    % draw vertical error bar
-    plot3(x(:), y(:), zMin(:), '-k');
-    plot3(x(:), y(:), zMax(:), '-k');
-    set(h, 'LineWidth', 2);
+    
+    if ~isempty(findall(0,'Type','figure'))
+        viewAngle = get(gca,'CameraViewAngle');
+        cameraPos = get(gca,'CameraPosition');
+    end
+    
+    zMin = z(:) + e(:);
+    zMax = z(:) - e(:);
     
     % now we want to fit a surface to our data
     % the  0.25 and 0.1 define the density of the fit surface
     % adjust them to your liking
-    tt1=floor(min(min(x))):0.25:max(max(x));
-    tt2=floor(min(min(y))):0.1:max(max(y));
+    tt1=floor(min(min(x))):0.35:max(max(x));
+    tt2=floor(min(min(y))):0.35:max(max(y));
 
     % prepare for fitting the surface
     [xg,yg]=meshgrid(tt1,tt2);
 
     % fit the surface to the data; 
     % matlab has several choices for the fit;  below is "linear"
-    zg=griddata(x(:), y(:), z(:), xg,yg,'linear');
+    zg=griddata(x(:), y(:), z(:), xg,yg,'linear')/1e6;
+    zgMin=griddata(x(:), y(:), zMin(:), xg,yg,'linear');
+    zgMax=griddata(x(:), y(:), zMax(:), xg,yg,'linear');
     % draw the mesh on our plot
-    mesh(xg,yg,zg), xlabel('x axis'), ylabel('y axis'), zlabel('z axis')
+    surf(xg,yg,zg,'FaceLighting','gouraud','LineWidth',0.3), xlabel('Sensor columns'), ylabel('Sensor rows'), zlabel('Pressure (MPa)')
+    if exist('viewAngle','var')
+        set(gca,'CameraViewAngle',viewAngle,'CameraPosition',cameraPos);
+    end
+    axis tight 
+    axis normal
+%     mesh(xg,yg,zgMin)
+%     mesh(xg,yg,zgMax)
 end
 
