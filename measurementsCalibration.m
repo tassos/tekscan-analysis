a340bceb1f96c68d4308593795b496bb3ccce084
Tassos (nakano) Natsakis <tassos.natsakis@mech.kuleuven.be>
2013-09-27 14:58:05 +0200

    Detection of foot side and sensor position for necessary data flipping

--- measurementsCalibration.m
+++ measurementsCalibration.m
@@ -40,13 +40,26 @@ function measurementsCalibration
         [meanData,loads,index] = readCalibrationFiles(h,measPathName,0);
         [x] = calibrationCoeff(h,measPathName,meanData,loads,index);
     end
+    
+    % Loading foot side and position of the sensor for deciding whether
+    % flipping of the data is needed, so that all measurements are aligned
+    % in the same direction. If it is a Right foot and the sensor was
+    % upside Down, or if it's a left foot and the sensor was upside Up,
+    % flipping is needed.
+    load([measPathName 'footSide.mat'],'Right','upsideUp');
+    flip=xor(Right,upsideUp);
 
-    for i=1:size(measFileName,2)        
+    for i=1:size(measFileName,2)
         waitbar((i/size(measFileName,2)),h,'Calibrating measurement files');
         [data,sensit,spacing] = readTekscan([measPathName measFileName{i}]); %#ok<NASGU> The variable is actually used in the save function five lines below.
         
         calibratedData=polyval(x.(sensit),data);
-        calibratedData(calibratedData < 0) =0; %#ok<NASGU> The variable is actually used in the save function two lines below.
+        calibratedData(calibratedData < 0) =0;
+        if flip
+            for k=1:size(calibratedData,1)
+                calibratedData(k,:,:)=fliplr(squeeze(calibratedData(k,:,:)));
+            end
+        end
         fileName=strtrim(measFileName{i});
         save([measPathName 'Calibrated_' fileName(1:end-4) '.mat'],'calibratedData','spacing');
     end
