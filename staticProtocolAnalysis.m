cd045a1f049282654d71f831ad4177d848136803
Tassos (nakano) Natsakis <tassos.natsakis@mech.kuleuven.be>
2013-11-12 11:39:30 +0100

    Static protocol data analysis feature

---
+++ staticProtocolAnalysis.m
@@ -0,0 +1,60 @@
+function staticProtocolAnalysis
+%STATICPROTOCOLANALYSIS Analysing different TekScan static
+% protocol measurements
+% Function: Comparing the results of different static protocol
+% measurements. No inputs or outputs from this function. 
+    clear
+    close all force
+    clc
+    
+    %% Loading measurement files
+    
+    % Choose measurements files to load and compare
+    [measFileName,measPathName] = uigetfile('.mat','Select measurement files',...
+        'MultiSelect','on',OSDetection);
+    
+    pathRt = [measPathName '../Real-Time/'];
+    filesRt = dir([pathRt '*.tdms']);
+    
+    % If the array of filenames is not a cell, convert it (e.g. in case only one
+    % file is selected)
+    if ~iscell(measFileName)
+        if measFileName == 0
+            return
+        else
+            measFileName={measFileName};
+        end
+    end
+    
+    sRate=10;
+    
+    for i=1:size(measFileName,1);
+        fileID = regexp(measFileName{i},'(?<=Calibrated_)\w*','match');
+        indexRt = findFile([fileID{1},'_'],filesRt);
+        
+        if indexRt == 0
+            warndlg(['No Real-Time measurement was found for ' fileID{1} ' measurement file'],'!! Warning !!')
+            continue
+        end
+        
+        loadcelldata = TDMS_readTDMSFile([pathRt filesRt(indexRt).name]);
+        inputFile = loadcelldata.data{5};
+        
+        A = importdata(inputFile{1},'\t');
+        
+        load([measPathName measFileName{i}],'calibratedData','spacing','fileName');
+        
+        pressureData = zeros(size(A,1),size(calibratedData,2),size(calibratedData,3));%#ok<NODEF> This variable is loaded a few lines above
+        for j=1:size(A,1)-1
+            if (A(j+1,1)*sRate > size(calibratedData,1))
+                pressureData(j,:,:) = mean(calibratedData(A(j,1)*sRate+1:end,:,:),1);
+                pressureData(j+1:end,:,:)=[];
+                continue
+            else
+                pressureData(j,:,:) = mean(calibratedData(A(j,1)*sRate+1:A(j+1,1)*sRate,:,:),1);
+            end
+        end
+        
+        
+    end
+end
