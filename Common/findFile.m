4f373e56feb5cbbd460ebfa763985a80cb7481b3
Tassos (nakano) Natsakis <tassos.natsakis@mech.kuleuven.be>
2013-11-26 09:36:04 +0100

    Removed common folder

--- Common/findFile.m
+++
@@ -1,6 +0,0 @@
-function [index] = findFile(string, fileslist)
-    index = 0;
-    for i=1:size(fileslist,1)
-        if ~isempty(strfind(fileslist(i).name,string)); index=i; return; end;
-    end
-end
