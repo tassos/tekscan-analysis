61ebadbd8b05564cf86a13c33372ccc09ad648ff
Tassos Natsakis (nakano) <tassos.natsakis@mech.kuleuven.be>
2014-08-01 10:43:20 +0200

    Loading and storing data frames with the static protocol measurements

--- loadMeasurements.r
+++ loadMeasurements.r
@@ -7,36 +7,58 @@ options("max.print"=300)
 
 source('directory.r')
 
-choices<-c("PeakPressure","force","peakLocation","PeakPressure Talus")
+choices<-c("PeakPressure","force","peakLocation","PeakPressure Talus","StaticProtocol")
 group<-select.list(choices, preselect = NULL, multiple = TRUE, title = "Select which measurements to recreate", graphics = getOption("menu.graphics"))
+
+if ("StaticProtocol" %in% group) {
+	type = "_Static"
+	nonCounted = 3
+} else {
+	type = ""
+	nonCounted = 2
+}
 
 myFile<-list()
 for (i in 1:10) {
-	myFile[i]  <-paste(indir,"Tekscan_Data_Foot", 36+i ,".mat",sep='')
+	myFile[i]  <-paste(indir,"Tekscan_Data",type,"_foot", 36+i ,".mat",sep='')
 }
 data<-data.frame()
+fLevels<-data.frame()
 ppTArea<-data.frame()
 
 for (j in 1:length(myFile)) {
 	myData <- readMat(myFile[[j]])
 
-	groups <- dim(myData$Rdata)[1]-2
-	groupsT <- dim(myData$RdataT)[1]-2
+	groups <- dim(myData$Rdata)[1]-nonCounted
+	groupsT <- dim(myData$RdataT)[1]-nonCounted
 	foot <- myData$Rdata[[groups+1]][1]
 	dataTemp<-list()
 	dataTempT<-list()
+	fLevelsTemp<-list()
 
 	for (i in 1:groups) {
 		case<-dimnames(myData$Rdata)[[1]][[i]]
 		
 		if (case != 'empty') {
 			dimnames(myData$Rdata[[i]][[1]])[[1]]<-myData$Rdata[[i]][[2]]
-			dimnames(myData$Rdata[[i]][[1]])[[2]]<-1:100
+			dimnames(myData$Rdata[[i]][[1]])[[2]]<-1:ncol(myData$Rdata[[i]][[1]])
 			dimnames(myData$Rdata[[i]][[1]])[[3]]<-unlist(myData$Rdata[[groups+2]])
 			dataTemp[[i]]<-as.data.frame(as.table(myData$Rdata[[i]][[1]]))
 			dataTemp[[i]]$Case<-as.factor(case)
 			dataTemp[[i]]$Foot<-as.factor(foot)
+			if (type == '_Static') {
+				dimnames(myData$Rdata[[i]][[3]])[[1]]<-myData$Rdata[[i]][[2]]
+				dimnames(myData$Rdata[[i]][[3]])[[2]]<-1:ncol(myData$Rdata[[i]][[1]])
+				dimnames(myData$Rdata[[i]][[3]])[[3]]<-myData$Rdata[[groups+nonCounted]]
+				fLevelsTemp[[i]] <- as.data.frame(as.table(myData$Rdata[[i]][[3]]))
+				fLevelsTemp[[i]]$Case<-as.factor(case)
+				fLevelsTemp[[i]]$Foot<-as.factor(foot)
+			}
 		}
+	}
+	if (type == '_Static') {
+		fLevelsTemp<-ldply(fLevelsTemp,data.frame)
+		fLevels<-rbind(fLevels,fLevelsTemp)
 	}
 	dataTemp<-ldply(dataTemp, data.frame)
 	data<-rbind(data,dataTemp)
@@ -46,7 +68,7 @@ for (j in 1:length(myFile)) {
 		
 		if (case != 'empty') {
 			dimnames(myData$RdataT[[i]][[1]])[[1]]<-myData$RdataT[[i]][[2]]
-			dimnames(myData$RdataT[[i]][[1]])[[2]]<-1:100
+			dimnames(myData$RdataT[[i]][[1]])[[2]]<-1:ncol(myData$RdataT[[i]][[1]])
 			dimnames(myData$RdataT[[i]][[1]])[[3]]<-unlist(myData$RdataT[[groupsT+2]])
 			dataTempT[[i]]<-as.data.frame(as.table(myData$RdataT[[i]][[1]]))
 			dataTempT[[i]]$Case<-as.factor(case)
@@ -58,10 +80,14 @@ for (j in 1:length(myFile)) {
 }
 rm(myData)
 
+colnames(fLevels) <- c("Trial","Percentage","Muscle","Activation","Case","Foot")
 colnames(data) <- c("Trial","Percentage","Variable","Value","Case","Foot")
 colnames(ppTArea) <- c("Trial","Percentage","Variable","Value","Case","Foot")
 data$Variable<-factor(data$Variable)
 ppTArea$Variable<-factor(ppTArea$Variable)
+if (type == "_Static") {
+	data<-merge(data, fLevels, by=c("Trial","Percentage","Case","Foot"))
+}
 
 if ("PeakPressure" %in% group) {
 	ppArea<-data[grep("(PeakPressure).",data$Variable),]
@@ -79,7 +105,7 @@ if ("PeakPressure" %in% group) {
 	pp$Foot<-factor(pp$Foot)
 	
 	save('pp',file=paste(outdir,'peakPressure.RData',sep=''))
-	save('ppArea',file=paste(outdir,'ppArea.RData',sep=''))
+	save('ppArea',file=paste(outdir,'ppArea',type,'.RData',sep=''))
 }
 
 if ("force" %in% group) {
@@ -91,7 +117,7 @@ if ("force" %in% group) {
 	force$Trial<-factor(force$Trial)
 	force$Foot<-factor(force$Foot)
 	
-	save('force',file=paste(outdir,'force.RData',sep=''))
+	save('force',file=paste(outdir,'force',type,'.RData',sep=''))
 }
 
 if ("peakLocation" %in% group) {
@@ -103,7 +129,7 @@ if ("peakLocation" %in% group) {
 	peakL$Trial<-factor(peakL$Trial)
 	peakL$Foot<-factor(peakL$Foot)
 	
-	save('peakL',file=paste(outdir,'peakL.RData',sep=''))
+	save('peakL',file=paste(outdir,'peakL',type,'.RData',sep=''))
 }
 
 if ("PeakPressure Talus" %in% group) {
@@ -113,7 +139,6 @@ if ("PeakPressure Talus" %in% group) {
 	ppTArea$Cols<-factor(ppTArea$Cols)
 	
 	#Cleaning up of bad or non used measurements
-	ppTArea<-ppTArea[grep("(foot39)|foot(44)|(foot46)",ppTArea$Foot,invert=T),]
 	ppTArea$Variable<-factor(ppTArea$Variable)
 	ppTArea$Trial<-factor(ppTArea$Trial)
 	ppTArea$Foot<-factor(ppTArea$Foot)
