6bab8c409dca5c5b45a1be9ea76889c552342ebb
Tassos Natsakis (nakano) <tassos.natsakis@mech.kuleuven.be>
2014-09-04 13:18:56 +0200

    Determining the directories based on the OS

--- directory.r
+++ directory.r
@@ -1,2 +1,10 @@
-outdir='~/Documents/PhD/Foot-ankle/RData/'
-indir='~/Foot-ankle/Measurements/Voet 99/Results/'
+OS <- .Platform$OS.type
+
+if (OS == "windows") {
+	outdir='~/PhD/Foot-ankle project/Measurements/Results Html/'
+	indir='~/PhD/Foot-ankle project/Measurements/Voet 99/Results/'
+}
+if (OS =="linux") {
+	outdir='~/Documents/PhD/Foot-ankle/RData/'
+	indir='~/Foot-ankle/Measurements/Voet 99/Results/'
+}
