a3245330029f52bd59b2b5afe65527d606505629
Tassos 'nakano' Natsakis <github@natsakis.com>
2014-09-28 09:52:59 +0200

    Changing order of the significance table, adding a new variable for the output directory of graphs

--- R/directory.r
+++ R/directory.r
@@ -2,9 +2,11 @@ OS <- .Platform$OS.type
 
 if (OS == "windows") {
 	outdir='~/PhD/Foot-ankle project/Measurements/Results Html/'
+	outdirg='~/PhD/Submissions/Journals/Clinical Orthopaedics and Related Research/Figures/'
 	indir='~/PhD/Foot-ankle project/Measurements/Voet 99/Results/'
 }
 if (OS =="unix") {
 	outdir='~/Documents/PhD/Foot-ankle/RData/'
+	outdirg='~/Documents/PhD/Foot-ankle/Journals/Clinical Orthopaedics and Related Research/Figures/'
 	indir='~/Foot-ankle/Measurements/Voet 99/Results/'
 }
--- R/peakPressure.r
+++ R/peakPressure.r
@@ -18,7 +18,6 @@ load(paste(outdir,'ppTArea.RData',sep=''))
 load(paste(outdir,'peakPressureNeutral.RData',sep=''))
 load(paste(outdir,'forceArea.RData',sep=''))
 outLaTeX<-paste(outdir,"LaTeX/",sep='')
-outdir<-paste("~/PhD/Submissions/Journals/Clinical Orthopaedics and Related Research/Figures/",sep='')
 
 # Combining the data frames of the Tibia and Talus pressures and cleaning the bad data points
 ppTArea$Case<-"Tekscan Talus"
@@ -51,13 +50,12 @@ power.analysis$power<-ddply(power.analysis,.(Rows,Cols,Phase), function(x) pwr.2
 mppS <-ddply(subset(ppS,Case!='TA'),.(Foot,Case,Trial), function(x) data.frame(Value=mean(x$Value/1E6)))
 
 # Statistics for detecting significant differences between Native and TAP
-wp<-ddply(mmppArea,.(Rows,Cols,Phase), function(x) data.frame(p.value=safewilTest(x,"Value","Case","TAA Tibia","Native Tibia")$p.value,estimated.difference=safewilTest(x,"Value","Case","TAA Tibia","Native Tibia")$estimate))
-wp$p.star<-ifelse(wp$p.value <=pLevel,"*"," ")
+wp<-ddply(mmppArea,.(Rows,Cols,Phase), function(x) data.frame(estimated.difference=safewilTest(x,"Value","Case","TAA Tibia","Native Tibia")$estimate,p.value=safewilTest(x,"Value","Case","TAA Tibia","Native Tibia")$p.value))
+wp$p.value<-ifelse(wp$p.value <=pLevel,paste(round(wp$p.value,3),"(*)"),round(wp$p.value,3))
 
-wpf<-ddply(mppArea,.(Foot,Rows,Cols,Phase), function(x) data.frame(p.value=safewilTest(x,"Value","Case","TAA Tibia","Native Tibia")$p.value,estim=safewilTest(x,"Value","Case","TAA Tibia","Native Tibia")$estimate))
-wpf$p.star<-ifelse(wpf$p.value <=pLevel,"*"," ")
+wpf<-ddply(mppArea,.(Foot,Rows,Cols,Phase), function(x) data.frame(estim=safewilTest(x,"Value","Case","TAA Tibia","Native Tibia")$estimate,p.value=safewilTest(x,"Value","Case","TAA Tibia","Native Tibia")$p.value))
+wpf$p.star<-ifelse(wpf$p.value <=pLevel,"(*)"," ")
 sumwpf<-ddply(wpf,.(Rows,Cols,Phase), function(x) data.frame(sign.inc=nrow(x[x$estim>0 & x$p.value<=pLevel,]),inc=nrow(x[x$estim>0 & x$p.value>pLevel,]),dec=nrow(x[x$estim<=0 & x$p.value>pLevel,]),sign.dec=nrow(x[x$estim<=0 & x$p.value<=pLevel,])))
-
 sumwpf<-merge(wp,sumwpf, intersect(c('Rows','Cols','Phase'),c('Rows','Cols','Phase')))
 
 sumTable<-xtable(sumwpf,caption='Summary of results',digits=4)#,align="rll|l|lcc|cccc")
@@ -65,7 +63,7 @@ sumLatex<-print(sumTable,include.rownames=F, print.results=F)
 write(insert.headers(sumLatex),paste(outLaTeX,"sumLatex.tex",sep=''))
 
 
-filename<-paste(outdir,"Area_Time_Talus")
+filename<-paste(outdirg,"Area_Time_Talus",sep='')
 svg(paste(filename,".svg",sep=''))
 p<-ggplot(subset(mmppArea, Case=="Native Talus"), aes(Phase, Value, fill=Case))+geom_boxplot(outlier.shape=NA)+
 	scale_y_continuous(name="Peak Pressure (MPa)")+scale_x_discrete(name="Stance phase percentage (%)")+
@@ -75,9 +73,9 @@ p<-ggplot(subset(mmppArea, Case=="Native Talus"), aes(Phase, Value, fill=Case))+
 print(p)
 dev.off()
 
-shell(paste("inkscape.exe -z -w 2000 -e",filename,".png ",filename,".svg"))
+system(paste("inkscape --export-png='",filename,".png ",filename,".svg'",sep=''))
 
-svg(paste(outdir,"Area_Time.svg",sep=''), width=8.4)
+svg(paste(outdirg,"Area_Time.svg",sep=''), width=8.4)
 p<-ggplot(subset(mmppArea, Case!="Native Talus"), aes(Phase, Value, fill=Case))+geom_boxplot(outlier.shape=NA)+
 	scale_y_continuous(name="Peak Pressure (MPa)")+scale_x_discrete(name="Stance phase percentage (%)")+
 	theme(axis.title=element_text(size=20),axis.text=element_text(colour='black', size=12),strip.text=element_text(size=12))+
@@ -86,7 +84,7 @@ print(p)
 dev.off()
 
 mppS$Case<-mapvalues(mppS$Case,from=c("Tekscan","TAP"), to=c("Native Tibia","TAA Tibia"))
-svg(paste(outdir,"Neutral_measurements.svg",sep=''), width=12)
+svg(paste(outdirg,"Neutral_measurements.svg",sep=''), width=12)
 p<-ggplot(subset(mppS,Foot!="foot38" & Foot!="foot43"), aes(Foot, Value, fill=Case))+geom_boxplot()+
 	theme(axis.title=element_text(size=20),axis.text=element_text(colour='black'))+
 	scale_y_continuous(name="Peak Pressure (MPa)")
