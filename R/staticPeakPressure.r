3964f156e2c290f538edb5dcdaa9e88b0b08e95e
Tassos Natsakis (nakano) <tassos.natsakis@mech.kuleuven.be>
2014-10-06 13:59:37 +0200

    Refactoring the code for readability and usability. Adding new graph for showing the trends of the CoP with increase of muscle forces

--- R/staticPeakPressure.r
+++ R/staticPeakPressure.r
@@ -1,8 +1,7 @@
 require(ggplot2)
 require(plyr)
 require(grid)
-require(xtable)
-library(lattice)
+require(tables)
 require(lme4)
 
 rm(list=ls())
@@ -19,81 +18,77 @@ labels<-c('Very (-)','Slightly (-)','Poorly (-)','Poorly (+)','Slightly (+)','Ve
 load(paste(outdir,'ppArea_Static.RData',sep=''))
 load(paste(outdir,'peakPressure_Static.RData',sep=''))
 load(paste(outdir,'CoP_Static.RData',sep=''))
+load(paste(outdir,'peakL_Static.Rdata',sep=''))
 outdirg=paste(outdirg,'Clinical Orthopaedics and Related Research/muscleActivation/Figures/',sep='')
-outLaTeX<-paste(outdirg,"LaTeX/",sep='')
 
-pp<-rbind(pp,CoP)
+pp<-rbind(pp,CoP,peakL)
 
 #Removing the two inactive muscles
 pp<-pp[grep('(Flex Hal)|(Flex Dig)',pp$Muscle, invert=T),]
-pp<-subset(pp,Case!='TA')
 pp$Case<-mapvalues(pp$Case,from=c("Tekscan","TAP","TA"), to=c("Native","TAA","TAA+TA"))
 pp$Phase<-mapvalues(pp$Phase,from=c("1","2","3"), to=c("Foot-flat","Mid-stance","Toe-off"))
+pp<-subset(pp,Case!='TAA+TA')
 pp<-pp[complete.cases(pp),]
 pp<-factorise(pp)
 
-#Removing the values for a muscle when it's not active
-pp$Activation<-round(pp$Activation,1)
-pp<-pp[pp$Activation<10,]
-
-pp$Activation<-factor(pp$Activation)
 #Finding the default value for each muscle, phase, case, foot and trial.
+pp$Activation<-factor(pp$Activation)
 pp<-ddply(pp,.(Foot,Case,Phase,Variable,Trial), function(x) data.frame(RawActiv=x$RawActiv, Muscle=x$Muscle, Activation=x$Activation, Value=x$Value, Default=unique(x[x$Percentage == min(as.character(x$Percentage)),]$Value)), .inform=T)
 pp<-ddply(pp,.(Foot,Case,Muscle,Phase,Variable,Trial,Activation), function(x) data.frame(RawActiv=mean(x$RawActiv), Value=mean(x$Value), Default=mean(x$Default)))
 pp$Activation<-as.numeric(as.character(pp$Activation))
-pp<-pp[pp$Activation!=1,]
-pp<-pp[complete.cases(pp),]
-pp<-factorise(pp)
+pp$Activation<-round(pp$Activation,1)
+pp<-pp[pp$Activation!=1 & pp$Activation<10,]
 
-npp<-ddply(pp,.(Foot,Case,Trial,Muscle,Phase,Variable), function(x) data.frame(Value=x$Value/x$Default, Activation=x$Activation))
+# Normalising the measured variable
+npp<-ddply(subset(pp,Variable=="PeakPressure"),.(Foot,Case,Trial,Muscle,Phase,Variable), function(x) data.frame(Value=x$Value/x$Default, Activation=x$Activation))
 
-# fm0<-dlply(pp,.(Variable,Muscle,Phase,Case), function(x) lmer(Value ~ RawActiv +(1| Foot),data = x))
-# fm1<-dlply(pp,.(Variable,Muscle,Phase,Case), function(x) lmer(scale(Value) ~ scale(RawActiv) +(1| Foot),data = x))
-fm2<-dlply(npp,.(Variable,Phase,Muscle,Case), function(x) lmer(Value ~ Activation +(1 | Foot),data = x))
+# Constructing linear mixed-effect models for each phase and muscle and case for the peak pressure as a response. The Activation is the fixed effect variable while the Foot is the random one.
+fm0<-factorise(fit_model(npp,"PeakPressure",0))
+fm1<-factorise(fit_model(pp,"CoP",1))
+fm2<-factorise(fit_model(pp,"PeakLocation",1))
 
-# fm0.coef<-ldply(fm0,function(x) fixef(x))
-# fm1.coef<-ldply(fm1,function(x) fixef(x))
-fm2.coef<-ldply(fm2,function(x) c(fixef(x),coef(summary(x))['Activation',]))
+# Merging the two location models
+names(fm1)[1]<-'Direction'
+fm1$Variable<-factor("Center of Pressure")
+names(fm2)[1]<-'Direction'
+fm2$Variable<-factor("Peak Location")
+fm12<-rbind(fm1,fm2)
 
 # Rounding off to two digits and changing the column names for nicer printing
-fm2.coef[,c(5,6,7,8,9)]<-round(fm2.coef[,c(5,6,7,8,9)],3)
-dimnames(fm2.coef)[[2]][c(5,8,9)]<-c('Intercept','Std.error','t.value')
-sumTable<-tabular((Phase*Case*Muscle)~Heading()*(identity)*(Estimate)*Heading()*Variable, data=fm2.coef)
-suppress<-latex(sumTable,paste(outLaTeX,"peakPressure.tex",sep=''),label='tab:Summary',digits=3)
+sumTablePP<-tabular(Phase*Case*Muscle~Heading()*identity*Activation*Heading()*Variable, data=fm0)
+suppress<-latex(sumTablePP,paste(outdirg,"LaTeX/peakPressure.tex",sep=''))
 
+sumTableCoP<-tabular(Phase*Case*Muscle~Variable*Direction*Heading()*identity*(Intercept+Activation), data=fm12)
+suppress<-latex(sumTableCoP,paste(outdirg,"LaTeX/Location.tex",sep=''))
+
+#Converting to a wide format, so that I can use the different variables for the aesthetics of the plot
+cop<-reshape(pp, idvar=c("Foot","Case","Trial","Phase","Muscle","Activation"), timevar="Variable", drop=c('Default','RawActiv'), direction="wide")
+dimnames(cop)[[2]][c(7:11)]<-c('PP','CoPAP','CoPML','PLAP','PLML')
+
+# Gathering the model estimates for drawing the predictor arrows
+fm1<-reshape(fm1, idvar=c("Phase","Muscle","Case"), timevar="Variable", direction="wide")
+fm2<-reshape(fm2., idvar=c("Phase","Muscle","Case"), timevar="Variable", direction="wide")
+
+# Defining height and width for the output figures and plotting
 height<-700
 width<-800
+res<-100
 
-png(paste(outdirg,"muscleEffect.png",sep=''), height=height, width=width, res=100)
-p<-ggplot(subset(npp,Variable=="PeakPressure"), aes(Activation, Value, color=Case))+geom_point()#+geom_abline(aes(intercept=Yintercept, slope=Slope, color=Case),data=regression)
-p<-p+geom_abline(aes(intercept=Intercept, slope=Estimate, color=Case), data=fm2.coef)
-p<-p+scale_y_continuous(name="Normalised Peak Pressure")
-p<-p+facet_grid(Muscle ~ Phase)
+png(paste(outdirg,"muscleEffect.png",sep=''), height, width, res=res)
+p<-ggplot(npp, aes(Activation, Value, color=Case))+geom_point()+
+	geom_abline(aes(intercept=Intercept, slope=Activation, color=Case), data=fm0)+
+	scale_y_continuous(name="Normalised Peak Pressure")+
+	theme(axis.title=element_text(size=20),axis.text=element_text(colour='black', size=12),strip.text=element_text(size=12))+
+	facet_grid(Muscle ~ Phase)
 print(p)
 dev.off()
 
-png(paste(outdirg,"muscleCoPAP.png",sep=''), height=height, width=width, res=100)
-p<-ggplot(subset(npp,Variable=="CoP A/P"), aes(Activation, Value, color=Case))+geom_point()#+geom_abline(aes(intercept=Yintercept, slope=Slope, color=Case),data=regression)
-p<-p+geom_abline(aes(intercept=Intercept, slope=Estimate, color=Case), data=fm2.coef)
-p<-p+scale_y_continuous(name="CoP A/P")
-p<-p+facet_grid(Muscle ~ Phase)
+png(paste(outdirg,"muscleCoP.png",sep=''), height, width, res=100)
+p<-ggplot(cop, aes(CoPML, CoPAP, color=Case))+geom_point(aes(alpha=PP))+
+	geom_segment(aes(x=Intercept.ML, y=Intercept.AP, xend=Intercept.ML+Activation.ML*10,
+	yend=Intercept.AP+Activation.AP*10), color=c("red",'blue'), size=1, data=fm1, arrow = arrow(length=unit(0.3,'cm')))+
+	scale_x_continuous(name="CoP medial-lateral",limits=c(-16,16))+scale_y_continuous(name="CoP anterior-posterior", limits=c(-23,23))+
+	theme(axis.title=element_text(size=20),axis.text=element_text(colour='black', size=12),strip.text=element_text(size=12))+
+	facet_grid(Muscle ~ Phase)
 print(p)
 dev.off()
-
-png(paste(outdirg,"muscleCoPML.png",sep=''), height=height, width=width, res=100)
-p<-ggplot(subset(npp,Variable=="CoP M/L"), aes(Activation, Value, color=Case))+geom_point()#+geom_abline(aes(intercept=Yintercept, slope=Slope, color=Case),data=regression)
-p<-p+geom_abline(aes(intercept=Intercept, slope=Estimate, color=Case), data=fm2.coef)
-p<-p+scale_y_continuous(name="CoP M/L")
-p<-p+facet_grid(Muscle ~ Phase)
-print(p)
-dev.off()
-
-# p<-ggplot(regression[regression$Variable=="PeakPressure",], aes(Slope, fill=Case))+geom_density(alpha=0.2)#+geom_histogram(binwidth=0.05, aes(y=..density..))
-# p<-p+facet_grid(Muscle~Phase)+xlim(-1,1)
-# print(p)
-
-# p<-ggplot(pp[grep("CoP M/L",pp$Variable),], aes(Activation, Value, color=Case))+geom_point()#+geom_abline(aes(intercept=Yintercept, slope=Slope, color=Case),data=regression)
-# p<-p+geom_smooth(aes(group=Case, fill=Case),method="lm", size=1.5, alpha=0.4)
-# p<-p+scale_y_continuous(name="Normalised Peak Pressure")
-# p<-p+facet_grid(Muscle ~ Phase)
-# print(p)
