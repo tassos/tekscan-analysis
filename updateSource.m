77b4a86e1ee85b06e224213637214f3d80a9296e
Tassos Natsakis (nakano) <tassos.natsakis@mech.kuleuven.be>
2015-02-11 15:27:01 +0100

    Making the updateSource mode robust and presenting a confirmation before actually updating

--- updateSource.m
+++ updateSource.m
@@ -1,12 +1,16 @@
 function updateSource
-    git('checkout lite');
-    result = git('pull origin lite');
-    if strfind(result,'Already up-to-date.')
-        msgbox('Software was already in the latest version');
-    elseif strfind(result,'fatal')||strfind(result,'error')
-        msgbox(sprintf(['There was a problem with the software ',...
-        'update.\nIt returned the following error:\n\n%s'],result));
-    else
-        msgbox('The newest version of the software was installed');
+    prompt='Are you sure you want to install the latest version? All changes you made in the source will be lost forever!';
+    if strcmp(questdlg(prompt,'Update source?','Yes','No','No'),'Yes');
+        git('checkout library');
+        git('reset --hard HEAD');
+        result = git('pull mech library');
+        if strfind(result,'Already up-to-date.')
+            msgbox('Software was already in the latest version');
+        elseif ~isempty(strfind(result,'fatal')) || ~isempty(strfind(result,'error')) || ~isempty(strfind(result,'git --help'))
+            msgbox(sprintf(['There was a problem with the software ',...
+            'update.\nIt returned the following error:\n\n%s'],result));
+        else
+            msgbox('The newest version of the software was installed');
+        end
     end
 end
